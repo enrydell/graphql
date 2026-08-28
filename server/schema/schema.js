4e1de3f9d11e9c8692efba1ea50aaeea810d7480
Henrique Dell <henriquedell1@gmail.com>
2026-08-28 14:52:47 -0300

    feat: Updated schema with relationships

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,29 +1,70 @@
 const graphql = require('graphql');
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = graphql;
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList
+} = graphql;
 
 const dummyBooks = [
-  { id: '1', name: 'The Great Gatsby', genre: 'Classic' },
-  { id: '2', name: '1984', genre: 'Dystopian' },
-  { id: '3', name: 'To Kill a Mockingbird', genre: 'Fiction' },
-  { id: '4', name: 'Brave New World', genre: 'Dystopian' },
-  { id: '5', name: 'The Catcher in the Rye', genre: 'Fiction' },
-  { id: '6', name: 'The Hobbit', genre: 'Fantasy' },
-  { id: '7', name: 'The Lord of the Rings', genre: 'Fantasy' },
+  { id: '1', name: 'The Great Gatsby', genre: 'Classic', authorId: '3' },
+  { id: '2', name: '1984', genre: 'Dystopian', authorId: '1' },
+  { id: '3', name: 'To Kill a Mockingbird', genre: 'Fiction', authorId: '2' },
+  { id: '4', name: 'Brave New World', genre: 'Dystopian', authorId: '1' },
+  { id: '5', name: 'The Catcher in the Rye', genre: 'Fiction', authorId: '2' },
+  { id: '6', name: 'The Hobbit', genre: 'Fantasy', authorId: '3' },
+  { id: '7', name: 'The Lord of the Rings', genre: 'Fantasy', authorId: '3' },
+];
+
+const dummyAuthors = [
+  { id: '1', name: 'George Orwell', age: 46 },
+  { id: '2', name: 'Harper Lee', age: 28 },
+  { id: '3', name: 'J.R.R. Tolkien', age: 56 }
 ];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
-  fields: {
+  fields: () => ({
     id: {
-      type: GraphQLString,
+      type: GraphQLID,
     },
     name: {
       type: GraphQLString,
     },
     genre: {
       type: GraphQLString,
+    },
+    author: {
+      type: AuthorType,
+      resolve(book) {
+        const authorId = book.authorId;
+        return dummyAuthors.find(author => author.id === authorId) || null;
+      }
     }
-  }
+  })
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: {
+      type: GraphQLID,
+    },
+    name: {
+      type: GraphQLString,
+    },
+    age: {
+      type: GraphQLInt,
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(author) {
+        return dummyBooks.filter(book => book.authorId === author.id);
+      }
+    }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -36,18 +77,33 @@ const RootQuery = new GraphQLObjectType({
       }
     },
     books: {
-      type: new graphql.GraphQLList(BookType),
+      type: new GraphQLList(BookType),
       resolve() {
         return dummyBooks;
       }
     },
     book: {
       args: {
-        id: { type: GraphQLString }
+        id: { type: GraphQLID }
       },
       type: BookType,
-      resolve(parent, args) {
+      resolve(_, args) {
         return dummyBooks.find(book => book.id === args.id) || null;
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve() {
+        return dummyAuthors;
+      }
+    },
+    author: {
+      args: {
+        id: { type: GraphQLID }
+      },
+      type: AuthorType,
+      resolve(_, args) {
+        return dummyAuthors.find(author => author.id === args.id) || null;
       }
     }
   }
